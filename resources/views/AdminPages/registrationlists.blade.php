2a343322e4fd5fb30346e0a96715d5c95f60bb14
trionell <gustav.rakeberg@gmail.com>
2019-06-08 15:45:22 +0200

    Added advocate name to registration list page

--- resources/views/AdminPages/registrationlists.blade.php
+++ resources/views/AdminPages/registrationlists.blade.php
@@ -54,6 +54,9 @@
                         <th class="tblheadcol" id="tbl-email">Epost</th>
                     @endcan
 
+                    <th class="tblheadcol" id="tbl-firstname-advocate">Förnamn Anhörig</th>
+                    <th class="tblheadcol" id="tbl-lastname-advocate">Efternamn Anhörig</th>
+
                     @can('contact_info_advocate')
                         <th class="tblheadcol" id="tbl-phone-advocate">Telefon Anhörig</th>
                         <th class="tblheadcol" id="tbl-email-advocate">Epost Anhörig</th>
@@ -137,6 +140,10 @@
                             <td class="tblheadcol" id="tbl-edit">{{$reg->phonenumber}}</td>
                             <td class="tblheadcol" id="tbl-edit">{{$reg->email}}</td>
                         @endcan
+
+                        <td class="tblheadcol" id="tbl-edit">{{$reg->first_name_advocate}}</td>
+                        <td class="tblheadcol" id="tbl-edit">{{$reg->last_name_advocate}}</td>
+
                         @can('contact_info_advocate')
                             <td class="tblheadcol" id="tbl-edit">{{$reg->phone_number_advocate}}</td>
                             <td class="tblheadcol" id="tbl-edit">{{$reg->email_advocate}}</td>
